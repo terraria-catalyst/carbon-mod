f8e8a8acebf87abe42e54630c4a01afc6ad55ee0
Ariame <gayforunova@gmail.com>
2024-04-28 18:55:15 -0500

    de-pointered

--- src/Carbon/Core/Particles/ParticleDefinition.cs
+++ src/Carbon/Core/Particles/ParticleDefinition.cs
@@ -2,16 +2,16 @@
 using TeamCatalyst.Carbon.Core.Autoloading;
 
 namespace TeamCatalyst.Carbon.Core.Particles {
-    internal unsafe abstract class ParticleDefinition : Definition {
+    internal abstract class ParticleDefinition : Definition {
 
         public abstract SpritebatchSettings spritebatchSettings { get; protected set; }
 
-        public abstract void Update(Particle* particle);
+        public abstract void Update(ref Particle particle);
         /// <summary>
         /// Spritebatch has already begun here with your <see cref="spritebatchSettings"/>!
         /// </summary>
         /// <param name="particle"></param>
         /// <param name="spriteBatch"></param>
-        public abstract void Draw(Particle* particle, SpriteBatch spriteBatch);
+        public abstract void Draw(in Particle particle, SpriteBatch spriteBatch);
     }
 }
--- src/Carbon/Core/Particles/ParticleSystem.cs
+++ src/Carbon/Core/Particles/ParticleSystem.cs
@@ -6,23 +6,23 @@ using System.Linq;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace TeamCatalyst.Carbon.Core.Particles {
-    public unsafe static class ParticleSystem {
+    public static class ParticleSystem {
         public const int MAXPARTICLES = 10000;
         static Particle[] _particles = new Particle[MAXPARTICLES];
-        static List<Particle> particlesToSort = new();
+        static List<Particle> particlesToSort = [];
 
-        public static Particle* FindNextEmptyParticle() {
-            Particle* particle = null;
+        public static ref Particle FindNextEmptyParticle() {
+            ref Particle particle = ref Particle.FailParticle;
 
             for (int i = 0; i < _particles.Length; i++) {
-                var partiterator = _particles[i];
+                ref var partiterator = ref _particles[i];
                 if (partiterator.lifeTime == 0) {
-                    particle = &partiterator;
+                    particle = partiterator;
                     break;
                 }
             }
 
-            return particle;
+            return ref particle;
         }
 
         static Dictionary<int, ParticleDefinition> _cachedDefinitions = new();
@@ -30,29 +30,28 @@ namespace TeamCatalyst.Carbon.Core.Particles {
         internal static void UpdateParticles() {
             particlesToSort.Clear(); // this probably gets rid of most of my benefits, will see about replacing it later
 
-            for (int i = 0; i <  MAXPARTICLES; i++) {
-                fixed(Particle* particle = &_particles[i]) {
-                    int defID = particle->definition;
+            for (int i = 0; i < MAXPARTICLES; i++) {
+                Particle particle = _particles[i];
+                int defID = particle.definition;
 
-                    if (defID == 0)
+                if (defID == 0)
+                    continue;
+
+                if (_cachedDefinitions.TryGetValue(defID, out var definition)) {
+                    definition.Update(ref particle);
+                }
+                else {
+                    // Slow update, to be avoided
+                    definition = GetDefinitionFromID(defID) as ParticleDefinition;
+
+                    if (definition is null)
                         continue;
 
-                    if (_cachedDefinitions.TryGetValue(defID, out var definition)) {
-                        definition.Update(particle);
-                    }
-                    else {
-                        // Slow update, to be avoided
-                        definition = GetDefinitionFromID(defID) as ParticleDefinition;
-
-                        if (definition is null)
-                            continue;
-
-                        _cachedDefinitions.Add(defID, definition);
-                        definition.Update(particle);
-                    }
-
-                    particlesToSort.Add(*particle);
+                    _cachedDefinitions.Add(defID, definition);
+                    definition.Update(ref particle);
                 }
+
+                particlesToSort.Add(particle);
             }
 
             particlesToSort = particlesToSort.OrderBy(p => p.definition).ToList();
@@ -62,26 +61,25 @@ namespace TeamCatalyst.Carbon.Core.Particles {
             var tempSettings = new SpritebatchSettings();
             bool activeSB = false;
             for (int i = 0; i < MAXPARTICLES; i++) {
-                fixed (Particle* particle = &_particles[i]) {
-                    int defID = particle->definition;
+                Particle particle = _particles[i];
+                ref int defID = ref particle.definition;
 
-                    if (defID == 0)
-                        continue;
+                if (defID == 0)
+                    continue;
 
-                    if (_cachedDefinitions.TryGetValue(defID, out var definition)) {
-                        if (definition.spritebatchSettings.GetHashCode() != tempSettings.GetHashCode()) { // Could maybe use a better method of comparison?
-                            tempSettings = definition.spritebatchSettings;
+                if (_cachedDefinitions.TryGetValue(defID, out var definition)) {
+                    if (definition.spritebatchSettings.GetHashCode() != tempSettings.GetHashCode()) { // Could maybe use a better method of comparison?
+                        tempSettings = definition.spritebatchSettings;
 
-                            if (activeSB) {
-                                spriteBatch.End();
-                            }
-
-                            spriteBatch.Begin(tempSettings.sortMode, tempSettings.blendState, tempSettings.samplerState, tempSettings.depthStencilState, tempSettings.rasterizerState, tempSettings.effect, tempSettings.transformationMatrix);
-                            activeSB = true;
+                        if (activeSB) {
+                            spriteBatch.End();
                         }
 
-                        definition.Draw(particle, spriteBatch);
+                        spriteBatch.Begin(tempSettings.sortMode, tempSettings.blendState, tempSettings.samplerState, tempSettings.depthStencilState, tempSettings.rasterizerState, tempSettings.effect, tempSettings.transformationMatrix);
+                        activeSB = true;
                     }
+
+                    definition.Draw(particle, spriteBatch);
                 }
             }
         }
@@ -94,6 +92,8 @@ namespace TeamCatalyst.Carbon.Core.Particles {
     }
 
     public struct Particle {
+        public static Particle FailParticle = new Particle() { definition = -1 };
+
         public Vector3 position;
         public Vector3 velocity;
         public uint lifeTime;
