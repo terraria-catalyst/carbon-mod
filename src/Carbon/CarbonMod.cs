db4d87d4adc9599b2dcb10f10ad27b608bdebb66
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2024-05-01 17:33:36 +0300

    create carbon folder

--- src/Carbon/CarbonMod.cs
+++ src/Carbon/CarbonMod.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -8,6 +10,7 @@ using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
 using ReLogic.Content.Sources;
 using TeamCatalyst.Carbon.Core;
+using Terraria;
 using Terraria.ModLoader;
 using Terraria.ModLoader.Core;
 
@@ -23,6 +26,9 @@ namespace TeamCatalyst.Carbon
     ///     Carbon <see cref="Mod"/> entrypoint.
     /// </summary>
     public sealed class CarbonMod : Mod {
+        public static readonly string CarbonFolder = Path.Join(Main.SavePath, "CarbonMod");
+        public static readonly string ModuleConfigFile = Path.Join(Main.SavePath, "CarbonMod", "enabled-modules.json");
+
         public static Mod? ModReference { get; private set; }
 
         private static ILHook? getLoadableTypesHookAutoload;
@@ -33,7 +39,7 @@ namespace TeamCatalyst.Carbon
 
         public override void Unload() {
             base.Unload();
-
+            
             getLoadableTypesHookAutoload?.Dispose();
             getLoadableTypesHookAutoload = null;
             getLoadableTypesHookAutoloadConfig?.Dispose();
@@ -42,7 +48,12 @@ namespace TeamCatalyst.Carbon
 
 #pragma warning disable CA2255
         [ModuleInitializer]
-        internal static void Initialize() {
+        internal static void Initialize() { 
+            if (!System.IO.File.Exists(CarbonFolder))
+            {
+                Directory.CreateDirectory(CarbonFolder);
+            }
+
             getLoadableTypesHookAutoload = new ILHook(typeof(Mod).GetMethod("Autoload", BindingFlags.NonPublic | BindingFlags.Instance)!, LoadableTypesHook);
             getLoadableTypesHookAutoloadConfig = new ILHook(typeof(Mod).GetMethod("AutoloadConfig", BindingFlags.NonPublic | BindingFlags.Instance)!, LoadableTypesHook);
         }
@@ -63,7 +74,17 @@ namespace TeamCatalyst.Carbon
             if (AssemblyLoadContext.GetLoadContext(typeof(CarbonMod).Assembly) is not AssemblyManager.ModLoadContext mlc)
                 throw new InvalidOperationException("CarbonMod is not loaded in a ModLoadContext.");
 
-            return mlc.loadableTypes.SelectMany(x => x.Value).ToArray();
+
+
+            List<Type> types = new List<Type>();
+
+            foreach (KeyValuePair<Assembly, Type[]> pair in mlc.loadableTypes)
+            {
+                Console.WriteLine(pair.Key.FullName);
+
+                types.AddRange(pair.Value);
+            }
+            return types.ToArray();
         }
 
         public override IContentSource CreateDefaultContentSource()
