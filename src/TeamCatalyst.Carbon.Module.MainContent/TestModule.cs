572c6a8edaca84c5d1807817dc92c9484dd52ff5
Ariame <gayforunova@gmail.com>
2024-05-01 22:07:31 -0500

    uhhh main content module

---
+++ src/TeamCatalyst.Carbon.Module.MainContent/TestModule.cs
@@ -0,0 +1,12 @@
+﻿using TeamCatalyst.Carbon.API;
+using Terraria.ModLoader;
+
+namespace TeamCatalyst.Carbon.Module.MainContent {
+    public sealed class TestModule : ICarbonModule {
+        void ILoadable.Load(Mod mod) {
+            mod.Logger.Info("Hello from TestModule!");
+        }
+
+        void ILoadable.Unload() { }
+    }
+}
